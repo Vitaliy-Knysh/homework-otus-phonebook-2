ab1de6dc91aff4ae3d4b123749ecc7c33df874da
Vitaliy <mr.knyshjr2@gmail.com>
2025-03-05 18:30:23 +0300

    тесты на класс Phonebook

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import os
+
 import pytest
+
+from app.model import Contact, Phonebook
 
 
 @pytest.fixture()
@@ -9,3 +13,27 @@ def valid_phone_numbers():
         '+7-111-222-33-44',
         '8-111-222-3344'
     )
+
+
+@pytest.fixture()
+def dummy_contacts():
+    return [
+        Contact(
+            name='name1',
+            phone_number='81112223344',
+            comment=''
+        ),
+        Contact(
+            name='name2',
+            phone_number='+71112223344',
+            comment='comment2'
+        ),
+    ]
+
+
+@pytest.fixture()
+def setup_teardown_file():
+    if not os.path.exists('phonebook.json'):
+        open("phonebook.json", "w").close()
+    yield
+    os.remove('phonebook.json')
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,39 @@
-from model import Contact
+from app.model import Contact, Phonebook
+import os
+
+from tests.conftest import setup_teardown_file
 
 
-def test_contact(valid_phone_numbers):
+def test_contact(valid_phone_numbers, setup_teardown_file):
     for number in valid_phone_numbers:
         Contact(
             phone_number=number,
             name='name',
-            # comment='comment'
         )
+
+
+class TestPhonebook:
+    @staticmethod
+    def test_phonebook_exists(setup_teardown_file):
+        pb_exists = os.path.exists('phonebook.json')
+        assert pb_exists
+
+    @staticmethod
+    def test_get_empty_contact_list(setup_teardown_file):
+        contacts = Phonebook.get_all_contacts()
+        assert contacts == []
+
+    @staticmethod
+    def test_add_contact(setup_teardown_file):
+        pass
+
+    @staticmethod
+    def test_remove_contact(setup_teardown_file):
+        pass
+
+    @staticmethod
+    def test_get_contact_list(setup_teardown_file):
+        pb = Phonebook()
+        for contact in dummy_contacts:
+            pb.add_contact(contact)
+        assert pb.get_all_contacts() == dummy_contacts
